16e968aeea68ce5a6f2e873c9c5e73af276e71ef
sunnyraj84348 <sunnyraj84348@gmail.com>
2018-11-09 09:42:35 +0700

    riva: nos bringup

--- nitrogen_riva.mk
+++ nitrogen_riva.mk
@@ -21,7 +21,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit some common LineageOS stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/nitrogen/products/common.mk)
 
 # Inherit from riva device
 $(call inherit-product, $(DEVICE_PATH)/device.mk)
@@ -30,7 +30,7 @@ PRODUCT_BRAND := Xiaomi
 PRODUCT_DEVICE := riva
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_MODEL := Redmi 5A
-PRODUCT_NAME := lineage_riva
+PRODUCT_NAME := nitrogen_riva
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
--- vendorsetup.sh
+++ vendorsetup.sh
@@ -21,5 +21,5 @@
 # function: add_lunch_combo generic-eng
 
 for var in eng user userdebug; do
-  add_lunch_combo lineage_riva-$var
+  add_lunch_combo nitrogen_riva-$var
 done
