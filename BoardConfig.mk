fabdf7ff410678ef1a3fa6de14f309b6fb470226
sunnyraj84348 <sunnyraj84348@gmail.com>
2018-12-27 16:19:39 +0000

    riva: Citrus Caf bringup

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -181,9 +181,6 @@ DEVICE_MATRIX_FILE   := $(DEVICE_PATH)/compatibility_matrix.xml
 # Hack for remove liblight
 $(shell rm -rf hardware/qcom/display-caf/msm8996/liblight)
 
-# Lineage Hardware
-JAVA_SOURCE_OVERLAYS := org.lineageos.hardware|$(DEVICE_PATH)/lineagehw|**/*.java
-
 # Tap2Wake
 TARGET_TAP_TO_WAKE_NODE := "/proc/gesture/onoff"
 
