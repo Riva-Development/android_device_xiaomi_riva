b3bf096bc7e0c13171717194276c5475a7856e8b
sunnyraj84348 <sunnyraj84348@gmail.com>
2018-12-28 02:10:20 +0000

    riva: disable telephony bootjar

--- device.mk
+++ device.mk
@@ -354,8 +354,8 @@ PRODUCT_PACKAGES += \
     telephony-ext \
     ims-ext-common
 
-PRODUCT_BOOT_JARS += \
-    telephony-ext
+#PRODUCT_BOOT_JARS += \
+#    telephony-ext
 
 # Seccomp policy
 PRODUCT_COPY_FILES += \
