ProtoDump/device_xiaomi_santoni

commit c1c24df08b46133e8e07ab24b7930f3ba781cf09

santoni: camera: Support Xiaomi makeup parameters
Change-Id: I1174266a3da15b474ee62d1ed100596ef55753c6

camera/QCamera2/HAL/QCameraParameters.cpp
@@ -492,6 +492,9 @@ const char QCameraParameters::KEY_QC_NOISE_REDUCTION_MODE_VALUES[] = "noise-redu
 const char QCameraParameters::KEY_TS_MAKEUP[] = "tsmakeup";
 const char QCameraParameters::KEY_TS_MAKEUP_WHITEN[] = "tsmakeup_whiten";
 const char QCameraParameters::KEY_TS_MAKEUP_CLEAN[] = "tsmakeup_clean";
+const char QCameraParameters::KEY_XM_MAKEUP[] = "xiaomi-still-beautify-values";
+const char QCameraParameters::KEY_XM_MAKEUP_WHITEN[] = "xiaomi-beauty-skin-color";
+const char QCameraParameters::KEY_XM_MAKEUP_CLEAN[] = "xiaomi-beauty-skin-smooth";
 #endif
 
 //KEY to share HFR batch size with video encoder.
@@ -4178,6 +4181,59 @@ int32_t QCameraParameters::setTsMakeup(const QCameraParameters& params)
     return NO_ERROR;
 }
 
+/*===========================================================================
+ * FUNCTION   : setXmMakeup
+ *
+ * DESCRIPTION: set setXmMakeup from user setting
+ *
+ * PARAMETERS :
+ *   @params  : user setting parameters
+ *
+ * RETURN     : int32_t type of status
+ *              NO_ERROR  -- success
+ *              none-zero failure code
+ *==========================================================================*/
+int32_t QCameraParameters::setXmMakeup(const QCameraParameters& params)
+{
+    const char *str = params.get(KEY_XM_MAKEUP);
+    const char *prev_str = get(KEY_XM_MAKEUP);
+
+    LOGH("str =%s & prev_str =%s", str, prev_str);
+
+    if (str != NULL) {
+        if (prev_str == NULL) {
+            m_bNeedRestart = true;
+        } else if (strcmp(str, prev_str) != 0) {
+            const char* prev_enabled = strrchr(prev_str, ':');
+            const char* curr_enabled = strrchr(str, ':');
+            if (prev_enabled && curr_enabled) {
+                prev_enabled++;
+                curr_enabled++;
+                m_bNeedRestart = (atoi(prev_enabled) > 0 != atoi(curr_enabled) > 0) ?
+                        true : m_bNeedRestart;
+            }
+            set(KEY_XM_MAKEUP, str);
+        }
+
+        str = params.get(KEY_XM_MAKEUP_WHITEN);
+        prev_str = get(KEY_XM_MAKEUP_WHITEN);
+        if (str != NULL) {
+            if (prev_str == NULL || strcmp(str, prev_str) != 0) {
+                set(KEY_XM_MAKEUP_WHITEN, str);
+            }
+        }
+
+        str = params.get(KEY_XM_MAKEUP_CLEAN);
+        prev_str = get(KEY_XM_MAKEUP_CLEAN);
+        if (str != NULL) {
+            if (prev_str == NULL || strcmp(str, prev_str) != 0) {
+                set(KEY_XM_MAKEUP_CLEAN, str);
+            }
+        }
+    }
+
+    return NO_ERROR;
+}
 #endif
 
 /*===========================================================================
@@ -5298,6 +5354,7 @@ int32_t QCameraParameters::updateParameters(const String8& p,
     if ((rc = updateFlash(false)))                      final_rc = rc;
 #ifdef TARGET_TS_MAKEUP
     if ((rc = setTsMakeup(params)))                     final_rc = rc;
+    if ((rc = setXmMakeup(params)))                     final_rc = rc;
 #endif
     if ((rc = setAdvancedCaptureMode()))                final_rc = rc;
 UPDATE_PARAM_DONE:
@@ -10509,15 +10566,29 @@ char* QCameraParameters::getParameters()
 bool QCameraParameters::getTsMakeupInfo(int &whiteLevel, int &cleanLevel) const
 {
     const char* pch_makeup_enable = get(QCameraParameters::KEY_TS_MAKEUP);
-    if (pch_makeup_enable == NULL) {
-        LOGH("pch_makeup_enable = null");
-        return false;
-    }
-    bool enableMakeup =
-            (strcmp(pch_makeup_enable,"On") == 0);
-    if (enableMakeup) {
-        whiteLevel = getInt(QCameraParameters::KEY_TS_MAKEUP_WHITEN);
-        cleanLevel = getInt(QCameraParameters::KEY_TS_MAKEUP_CLEAN);
+    bool enableMakeup = false;
+    if (pch_makeup_enable != NULL) {
+        enableMakeup =
+                (strcmp(pch_makeup_enable,"On") == 0);
+        if (enableMakeup) {
+            whiteLevel = getInt(QCameraParameters::KEY_TS_MAKEUP_WHITEN);
+            cleanLevel = getInt(QCameraParameters::KEY_TS_MAKEUP_CLEAN);
+        }
+    } else {
+        pch_makeup_enable = get(QCameraParameters::KEY_XM_MAKEUP);
+        if (pch_makeup_enable != NULL) {
+            const char* xm_makeup_enable = strrchr(pch_makeup_enable, ':');
+            if (xm_makeup_enable == NULL) {
+                return false;
+            }
+            xm_makeup_enable++;
+            enableMakeup = atoi(xm_makeup_enable) > 0;
+            LOGD("Xiaomi makeup enable %d", (int)enableMakeup);
+            if (enableMakeup) {
+                whiteLevel = getInt(QCameraParameters::KEY_XM_MAKEUP_WHITEN) * 10;
+                cleanLevel = getInt(QCameraParameters::KEY_XM_MAKEUP_CLEAN) * 10;
+            }
+        }
     }
     return enableMakeup;
 }

camera/QCamera2/HAL/QCameraParameters.h
@@ -615,6 +615,9 @@ private:
     static const char KEY_TS_MAKEUP[];
     static const char KEY_TS_MAKEUP_WHITEN[];
     static const char KEY_TS_MAKEUP_CLEAN[];
+    static const char KEY_XM_MAKEUP[];
+    static const char KEY_XM_MAKEUP_WHITEN[];
+    static const char KEY_XM_MAKEUP_CLEAN[];
 #endif
     //param key for HFR batch size
     static const char KEY_QC_VIDEO_BATCH_SIZE[];
@@ -954,6 +957,7 @@ private:
     int32_t setStillMore(const QCameraParameters& );
 #ifdef TARGET_TS_MAKEUP
     int32_t setTsMakeup(const QCameraParameters& );
+    int32_t setXmMakeup(const QCameraParameters& );
 #endif
     int32_t setNoiseReductionMode(const QCameraParameters& );
     int32_t setRedeyeReduction(const QCameraParameters& );

Riva-Development/android_device_xiaomi_riva

commit de86fb73aa97d42fe4e0bf562efb8ebf2af99723

riva: camera: Set KEY_XM_MAKEUP when change in value is detected
Fixes: c1c24df08b4 ("santoni: camera: Support Xiaomi makeup parameters")

Change-Id: I8bfc85ecca62ded30aaae107c1d2f0f84359e020

camera/QCamera2/HAL/QCameraParameters.cpp
@@ -4208,6 +4208,7 @@ int32_t QCameraParameters::setXmMakeup(const QCameraParameters& params)
     if (str != NULL) {
         if (prev_str == NULL) {
             m_bNeedRestart = true;
+            set(KEY_XM_MAKEUP, str);
         } else if (strcmp(str, prev_str) != 0) {
             const char* prev_enabled = strrchr(prev_str, ':');
             const char* curr_enabled = strrchr(str, ':');