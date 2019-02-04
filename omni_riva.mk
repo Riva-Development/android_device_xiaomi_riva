ee1956f7446318daa16827758ef5b3d1943e37f9
sunnyraj84348 <sunnyraj84348@gmail.com>
2019-02-04 02:39:11 +0000

    riva: Omni bringup

--- omni_riva.mk
+++ omni_riva.mk
@@ -18,19 +18,24 @@ DEVICE_PATH := device/xiaomi/riva
 
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
 
 # Inherit some common LineageOS stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/omni/config/common.mk)
 
 # Inherit from riva device
 $(call inherit-product, $(DEVICE_PATH)/device.mk)
+
+# Omni
+TARGET_BOOTANIMATION_SIZE := 720p
+PRODUCT_PACKAGE_OVERLAYS += vendor/omni/overlay/CarrierConfig
+$(call inherit-product, vendor/omni/config/gsm.mk)
 
 PRODUCT_BRAND := Xiaomi
 PRODUCT_DEVICE := riva
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_MODEL := Redmi 5A
-PRODUCT_NAME := lineage_riva
+PRODUCT_NAME := omni_riva
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
